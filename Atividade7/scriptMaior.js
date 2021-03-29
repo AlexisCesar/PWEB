d2bfe26c33fb70fdf661c06ab50cfdd605e58dea
Alexis Cesar <42656077+AlexisCesar@users.noreply.github.com>
2021-03-29 11:53:59 -0300

    Arrumado o comparador da função

--- Atividade7/scriptMaior.js
+++ Atividade7/scriptMaior.js
@@ -1,9 +1,9 @@
 alert("Vamos ver qual o maior valor (pico)!");
 
 function maior(num1, num2, num3) {
-  if(num1 > num2 && num1 > num3)
+  if(num1 >= num2 && num1 >= num3)
   	return num1;
-  else if(num2 > num1 && num2 > num3)
+  else if(num2 >= num1 && num2 >= num3)
   	return num2;
   else
   	return num3;
