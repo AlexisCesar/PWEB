29c01a9f64e7ffdee39c8de94b710232107aec59
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-04-12 09:53:44 -0300

    Atividade 8 - Primeiro commit

---
+++ Atividade8/scriptObjeto.js
@@ -0,0 +1,22 @@
+alert("Criando objetos!"
+	+ "\nVerifique o código-fonte para ver"
+	+ "\nas formas utilizadas.");
+
+//Usando Construtor
+var aluno = new Object();
+aluno.nome = "Alexis Cesar Ruiz de Almeida";
+aluno.ra = "0030481921015";
+aluno.curso = "ADS";
+
+//Sem Construtor
+var aluno2 = {};
+aluno2.nome = "Maria";
+aluno2.ra = "0000000000001";
+aluno2.curso = "Sistemas Biomédicos";
+
+//Forma Literal
+var aluno3 = {
+	nome: "Angelo",
+	ra: "0000000000002",
+	curso: "Manufatura Avançada"
+};
