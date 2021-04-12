051de34dd500cde78a1777e2f274c2c7859d865f
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-04-12 10:02:47 -0300

    Update - create constructor and print tests

--- Atividade8/scriptObjeto.js
+++ Atividade8/scriptObjeto.js
@@ -3,10 +3,20 @@ alert("Criando objetos!"
 	+ "\nas formas utilizadas.");
 
 //Usando Construtor
-var aluno = new Object();
-aluno.nome = "Alexis Cesar Ruiz de Almeida";
-aluno.ra = "0030481921015";
-aluno.curso = "ADS";
+function Aluno(nome, ra, curso){
+	this.nome = nome;
+	this.ra = ra;
+	this.curso = curso;
+}
+
+var aluno = new Aluno("Alexis Cesar Ruiz de Almeida",
+	"0030481921015",
+	"ADS");
+
+alert("Testando com construtor: "
+	+ "\nNome: " + aluno.nome
+	+ "\nRA: " + aluno.ra
+	+ "\nCurso: " + aluno.curso);
 
 //Sem Construtor
 var aluno2 = {};
@@ -14,9 +24,19 @@ aluno2.nome = "Maria";
 aluno2.ra = "0000000000001";
 aluno2.curso = "Sistemas Biomédicos";
 
+alert("Testando sem construtor: "
+	+ "\nNome: " + aluno2.nome
+	+ "\nRA: " + aluno2.ra
+	+ "\nCurso: " + aluno2.curso);
+
 //Forma Literal
 var aluno3 = {
 	nome: "Angelo",
 	ra: "0000000000002",
 	curso: "Manufatura Avançada"
 };
+
+alert("Testando forma literal: "
+	+ "\nNome: " + aluno3.nome
+	+ "\nRA: " + aluno3.ra
+	+ "\nCurso: " + aluno3.curso);
