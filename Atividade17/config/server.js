75f7a9077e0142897465dbe65806f203b5a64201
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-06-07 10:28:58 -0300

    Add consign

--- Atividade17/config/server.js
+++ Atividade17/config/server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 
+var consign = require('consign');
+
+var bodyParser = require('body-parser');
+
 var app = express(); // executando o express
 
 var path = require('path')
@@ -7,6 +11,12 @@ var path = require('path')
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.use(express.static(path.join('./', 'public')));
+
+consign({ cwd: './'})
+	.include('routes')
+	.into(app);
 
 module.exports = app;
